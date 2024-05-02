df7ad9ae6cf8726746ed370abc2bc118031fa575
Karukan0814 <46077409+Karukan0814@users.noreply.github.com>
2024-05-02 10:51:13 -0700

    postSeederプロト作成

---
+++ src/seeders/postSeeder.ts
@@ -0,0 +1,49 @@
+import prisma from "../lib/db";
+const bcrypt = require("bcryptjs");
+import { faker } from "@faker-js/faker";
+
+function generatePostText() {
+  const text = faker.lorem.paragraphs(); // ランダムな段落を生成
+  const trimmedText = text.slice(0, 200); // 200文字にトリム
+  return trimmedText;
+}
+function generateRandomImageUrl() {
+  const width = faker.datatype.number({ min: 100, max: 1000 });
+  const height = faker.datatype.number({ min: 100, max: 1000 });
+  return `https://picsum.photos/${width}/${height}`;
+}
+async function postSeeder() {
+  console.log("postSeeder_start");
+  const testParentPost = [];
+  const testUserList = await prisma.user.findMany();
+  for (let i = 0; i < 100; i++) {
+    if (testUserList.length > 0) {
+      // リストからランダムに1ユーザーを選択
+      const randomIndex = Math.floor(Math.random() * testUserList.length);
+      const randomUser = testUserList[randomIndex];
+
+      console.log(randomUser); // ランダムに選ばれたユーザーの情報を表示
+      testParentPost.push({
+        text: generatePostText(),
+        img: generateRandomImageUrl(),
+        userId: randomUser.id,
+      });
+    } else {
+      console.log("No users found in the database.");
+    }
+  }
+
+  console.log({ testParentPost });
+  try {
+    await prisma.post.createMany({
+      data: testParentPost,
+      skipDuplicates: true,
+    });
+    console.log("Posts seeded successfully.");
+  } catch (error) {
+    console.error("posts insert failed:", error);
+  }
+  console.log("Posts seeded");
+}
+
+export default postSeeder;
