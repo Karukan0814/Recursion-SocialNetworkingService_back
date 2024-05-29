30ee0afe02b4d7a7da75c4b37eaf47cd5347ce76
Karukan0814 <46077409+Karukan0814@users.noreply.github.com>
2024-05-29 10:16:17 -0700

    5minバッチにポスト含める

--- src/cron/fakeUsersBatch.ts
+++ src/cron/fakeUsersBatch.ts
@@ -1,13 +1,20 @@
 import cron from "node-cron";
 
 import prisma from "../lib/db";
-import { getRandomObject, registerNotification } from "../lib/util";
-import { NotificationType } from "@prisma/client";
+import {
+  generatePostText,
+  generateRandomImageUrl,
+  getRandomObject,
+  registerNotification,
+} from "../lib/util";
+import { NotificationType, Post, User } from "@prisma/client";
 import likeSeeder from "../seeders/likeSeeder";
 import {
   LIKES_PER_INFLUENCER,
   LIKES_PER_USER,
   NUMBER_OF_INFLUENCERS_FOLLOWERS,
+  POSTS_PER_USER,
+  REPLIES_PER_USER,
 } from "../seeders/seederConatants";
 import postSeeder from "../seeders/postSeeder";
 import replySeeder from "../seeders/replySeeder";
@@ -38,8 +45,8 @@ export async function dayBatchReply() {
 }
 
 export async function fiveMinBatch() {
-  // scheduledAtが現在時刻をすぎているPostを探して、sentAtに現在時刻を入れる
   try {
+    // scheduledAtが現在時刻をすぎているPostを探して、sentAtに現在時刻を入れる
     const now = new Date();
     const updatedCount = await prisma.post.updateMany({
       where: {
@@ -163,22 +170,130 @@ export async function sixHoursBatch() {
     await prisma.$disconnect();
   }
 }
-// 一日に一回のバッチ処理
 
-cron.schedule("0 8 * * *", async () => {
-  console.log("dayBatchPost");
-  await dayBatchPost();
-});
+export async function batch5minPost() {
+  try {
+    console.log("batch5minPost -start");
+    const testUserList = await prisma.user.findMany({
+      where: {
+        fakeFlag: true,
+      },
+    }); //登録されているフェイクのユーザーのリストを取得
 
-cron.schedule("0 12 * * *", async () => {
-  console.log("dayBatchReply");
-  await dayBatchReply();
-});
+    // 【要件】各ユーザーは毎日ランダムなテキストジェネレーターを使用した内容の異なる 3 つの投稿を行う
+    // →5分ごとのバッチのついでに１０人が３ポストすればいい
+
+    // 指定の数だけランダムにユーザーを取得する
+    const pickedUpUsers = getRandomObject<User>(testUserList, 10);
+
+    const newPosts: Array<any> = [];
+
+    //一日当たり指定の回数ポストするように設定
+    for (let user of pickedUpUsers) {
+      for (let i = 0; i < POSTS_PER_USER; i++) {
+        newPosts.push({
+          text: generatePostText(200),
+          img: generateRandomImageUrl(),
+          imgFileType: "image/jpeg", //piscumは通常、jpegファイルを返すため
+          userId: user.id,
+          sentAt: new Date(),
+        });
+      }
+    }
+
+    // 【要件】1つのランダムに選ばれたメインの投稿に対して返信をします。=毎日 1 つのランダムなメイン投稿に返信する
+    // →5分ごとのバッチのついでに１人がリプライすればいい
+
+    const replyUserList = getRandomObject<User>(testUserList, 1);
+    const toDate = new Date();
+    const fromDate = new Date(toDate.getTime() - 24 * 3600 * 1000); // 24時間前の日時
+
+    const testPostList: Post[] = await prisma.post.findMany({
+      where: {
+        sentAt: {
+          gte: fromDate, //24時間以内に投稿されたポスト
+          lt: toDate,
+        },
+        replyToId: null, //返信ではない、メインのポスト
+        NOT: {
+          sentAt: null,
+        },
+      },
+    });
+
+    // リプライしたいポストを選択
+    const replyToPostList = getRandomObject<Post>(testPostList, 1);
+    if (replyToPostList && replyToPostList.length > 0) {
+      const replyToPost = replyToPostList[0];
+      for (let user of replyUserList) {
+        for (let i = 0; i < REPLIES_PER_USER; i++) {
+          // リプライのデータをプッシュ
+          newPosts.push({
+            text: generatePostText(200),
+            img: generateRandomImageUrl(),
+            imgFileType: "image/jpeg", //piscumは通常、jpegファイルを返すため
+            replyToId: replyToPost.id,
+            userId: user.id,
+            scheduledAt: null,
+            sentAt: new Date(),
+          });
+
+          // 通知を登録
+          await registerNotification(
+            NotificationType.REPLY,
+            replyToPost.userId,
+            user.id,
+            replyToPost.id
+          );
+        }
+      }
+    }
+    // ポストを登録
+    const posts = await prisma.post.createMany({
+      data: newPosts,
+      skipDuplicates: true,
+    });
+
+    console.log(`${posts.count} of  posts were sent!`);
+  } catch (e: any) {
+    console.error(e);
+    process.exit(1);
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+export async function batchReplyPost() {
+  try {
+    // 【要件】各ユーザーは毎日ランダムなテキストジェネレーターを使用した内容の異なる 3 つの投稿を行う
+
+    // 【要件】1つのランダムに選ばれたメインの投稿に対して返信をします。=毎日 1 つのランダムなメイン投稿に返信する
+    await replySeeder();
+  } catch (e: any) {
+    console.error(e);
+    process.exit(1);
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+// // 一日に一回のバッチ処理
+
+// cron.schedule("0 8 * * *", async () => {
+//   console.log("dayBatchPost");
+//   await dayBatchPost();
+// });
+
+// cron.schedule("0 12 * * *", async () => {
+//   console.log("dayBatchReply");
+//   await dayBatchReply();
+// });
 
 // 5分に一回のバッチ処理
 cron.schedule("*/5 * * * *", async () => {
   console.log("fiveMinBatch");
   await fiveMinBatch();
+  await batch5minPost();
 });
 
 // 6時間ごとのバッチ処理
